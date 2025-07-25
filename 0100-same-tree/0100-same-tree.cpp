273203f253a013ba935dfb26d4185db36e2cba05
ankitpatidar3739 <patidarankit3739@gmail.com>
2025-07-25 22:47:03 +0530

    Time: 0 ms (100%), Space: 12.9 MB (48.21%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -14,7 +14,9 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p==NULL || q==NULL){
             return (p==q);
+        }else{
+            return (p->val==q->val) && isSameTree(p->right,q->right) && isSameTree(p->left,q->left);
         }
-        return(p->val==q->val) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);        
+       
     }
 };
