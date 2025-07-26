976d7028762a7f7a99b46091e9407e0631e5c7c7
ankitpatidar3739 <patidarankit3739@gmail.com>
2025-07-26 19:31:31 +0530

    Time: 20 ms (41.2%), Space: 26.5 MB (27.86%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
-        map<string,vector<string>>mpp;
+        unordered_map<string,vector<string>>mpp;
 
         for(int i=0;i<strs.size();i++){
             string s=strs[i];
