bb2230c794d684aefcfcbb629aa6d62a1711db60
ankitpatidar3739 <patidarankit3739@gmail.com>
2025-08-01 22:00:41 +0530

    Time: 0 ms (100%), Space: 13.2 MB (59.18%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
@@ -7,15 +7,14 @@ public:
 
         while(low<=high){
             int mid=(low+high)/2;
-            int missing=arr[mid]-(mid+1);
 
+            int missing=arr[mid]-(mid+1);
             if(missing<k){
                 low=mid+1;
-
             }else{
                 high=mid-1;
             }
         }
-        return k+high+1;
+        return high+1+k;
     }
 };
