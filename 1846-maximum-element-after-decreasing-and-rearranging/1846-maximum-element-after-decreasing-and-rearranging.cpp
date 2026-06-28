0c7d489d814fad26bd06ece4ed9098c23e4c74a4
ankitpatidar3739 <patidarankit3739@gmail.com>
2026-06-28 13:18:25 +0530

    Time: 12 ms (26.55%), Space: 55.4 MB (4.52%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -4,7 +4,7 @@ public:
         sort(arr.begin(),arr.end());
         int n=arr.size();
         arr[0]=1;
-        int maxi=1;
+        int maxi=arr[0];
         for(int i=1;i<n;i++){
             if(abs(arr[i]-arr[i-1])>1){
                 arr[i]=arr[i-1]+1;
