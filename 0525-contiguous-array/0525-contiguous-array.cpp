0d7b471c7333749c58fc3813ef75e07f0773260c
ankitpatidar3739 <patidarankit3739@gmail.com>
2026-07-07 01:25:02 +0530

    Time: 28 ms (32.25%), Space: 88.2 MB (72.9%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        unordered_map<int,int>mpp;
+        mpp[0]=-1;
+        int sum=0;
+        int maxi=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                sum+=-1;
+            }else{
+                sum+=1;
+            }
+            if(mpp.find(sum)!=mpp.end()){
+                maxi=max(maxi,i-mpp[sum]);
+            }else{
+                mpp[sum]=i;
+            }
+        }
+        return maxi;
+    }
+};
