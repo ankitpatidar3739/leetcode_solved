cdb76f60b70d2bd0a7941cb59610568ef1dfce7a
ankitpatidar3739 <patidarankit3739@gmail.com>
2026-07-17 16:27:51 +0530

    Time: 0 ms (100%), Space: 8.5 MB (25.38%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int func(int n,vector<int>& dp){
+        if(n<0) return 0;
+        if(n==1) return 1;
+        if(n==2) return 1;
+
+        if(dp[n]!=-1) return dp[n];
+
+        return dp[n]=func(n-1,dp)+func(n-2,dp)+func(n-3,dp);
+    }
+    int tribonacci(int n) {
+        vector<int>dp(n+1,-1);
+
+        return func(n,dp);
+    }
+};
