ed4465e2cdc24821090dd0c188e273bbc0bd2804
ankitpatidar3739 <patidarankit3739@gmail.com>
2026-07-30 18:33:42 +0530

    Time: 0 ms (100%), Space: 10.3 MB (74.54%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,36 +1,27 @@
 class Solution {
-    private:
-    int func(vector<int>&num){
-        if (num.empty()) {
-            return 0;
-        }
-        int prev=num[0];
+public:
+    int rob1(vector<int>& nums) {
+        int n=nums.size();
+        int prev=nums[0];
         int prev2=0;
-
-        for(int i=1;i<num.size();i++){
-            int take=num[i];
-            if(i>1) take+=prev2;
-
-            int nottake=0+prev;
-
+        for(int i=1;i<n;i++){
+            int take=nums[i];if(i>1) take+=prev2;
+            int nottake=prev;
             int curr=max(take,nottake);
             prev2=prev;
-            prev=curr; 
+            prev=curr;   
         }
         return prev;
     }
-public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-         if (n == 1) {
-            return nums[0];
-        }
         vector<int>temp1,temp2;
-
-        for(int i=0;i<nums.size();i++){
+        if(n==1) return nums[0];
+        for(int i=0;i<n;i++){
             if(i!=0) temp1.push_back(nums[i]);
             if(i!=n-1) temp2.push_back(nums[i]);
         }
-        return max(func(temp1),func(temp2));
+        return max(rob1(temp1),rob1(temp2));
+
     }
 };
