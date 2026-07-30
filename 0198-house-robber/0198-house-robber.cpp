527fd292b960b216d5a1c2e3676bc42b3612f5a4
ankitpatidar3739 <patidarankit3739@gmail.com>
2026-07-30 17:08:15 +0530

    Time: 0 ms (100%), Space: 10.6 MB (47.28%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,30 +1,20 @@
 class Solution {
-    private:
-    long long func(vector<int>&num){
-        int prev=num[0];
-        int prev2=0;
-
-        for(int i=1;i<num.size();i++){
-            int take=num[i]+prev2;
-            // if(i>1) take+=prev2;
-
-            int nottake=0+prev;
-
-            int curr=max(take,nottake);
-            prev2=prev;
-            prev=curr; 
-        }
-        return prev;
-    }
 public:
+
+    int f(int idx,vector<int>& nums,vector<int>& dp){
+        if(idx==0) return nums[idx];
+        if(idx<0) return 0;
+        if(dp[idx]!=-1) return dp[idx];
+
+        int pick=nums[idx]+f(idx-2,nums,dp);
+        int notpick=f(idx-1,nums,dp);
+
+        return dp[idx]=max(pick,notpick);
+    }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        // vector<int>temp1,temp2;
+        vector<int>dp(n,-1);
 
-        // for(int i=0;i<nums.size();i++){
-        //     if(i!=0) temp1.push_back(nums[i]);
-        //     if(i!=n-1) temp2.push_back(nums[i]);
-        // }
-        return func(nums);
+        return f(n-1,nums,dp);
     }
 };
