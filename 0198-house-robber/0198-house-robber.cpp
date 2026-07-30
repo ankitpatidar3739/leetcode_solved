e1ed309a03436f4e8c1eaf529252e4580c1353cc
ankitpatidar3739 <patidarankit3739@gmail.com>
2026-07-30 18:18:23 +0530

    Time: 0 ms (100%), Space: 10.6 MB (47.28%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
 
-    int f(int idx,vector<int>& nums,vector<int>& dp){
-        if(idx==0) return nums[idx];
-        if(idx<0) return 0;
-        if(dp[idx]!=-1) return dp[idx];
+    // int f(int idx,vector<int>& nums,vector<int>& dp){
+    //     if(idx==0) return nums[idx];
+    //     if(idx<0) return 0;
+    //     if(dp[idx]!=-1) return dp[idx];
 
-        int pick=nums[idx]+f(idx-2,nums,dp);
-        int notpick=f(idx-1,nums,dp);
+    //     int pick=nums[idx]+f(idx-2,nums,dp);
+    //     int notpick=f(idx-1,nums,dp);
 
-        return dp[idx]=max(pick,notpick);
-    }
+    //     return dp[idx]=max(pick,notpick);
+    // }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,-1);
+        vector<int>dp(n);
 
-        return f(n-1,nums,dp);
+        // return f(n-1,nums,dp);
+        dp[0]=nums[0];
+
+        for(int i=1;i<n;i++){
+            int take=nums[i];if(i>1) take+=dp[i-2];
+            int nottake=dp[i-1];
+            dp[i]=max(take,nottake);
+        }
+        return dp[n-1];
     }
 };
