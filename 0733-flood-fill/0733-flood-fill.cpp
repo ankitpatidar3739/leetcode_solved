0014f43740fd2fe5d541e2143efff99583ae0815
ankitpatidar3739 <patidarankit3739@gmail.com>
2026-09-05 20:05:12 +0530

    Time: 0 ms (100%), Space: 18.5 MB (19.74%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,25 +1,39 @@
 class Solution {
-    private:
-    void dfs(int row,int col,vector<vector<int>>&ans,vector<vector<int>>&image,int newColor,int delRow[],int delCol[],int iniColor){
-        ans[row][col]=newColor;
+public:
+    typedef pair<int,int>p;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n=image.size();
         int m=image[0].size();
+        int original = image[sr][sc];
+        if(original == color) return image;
+        vector<vector<int>> vis(n,vector<int>(m,-1));
+        image[sr][sc]=color;
+        vis[sr][sc]=1;
 
-        for(int i=0;i<4;i++){
-            int nrow=row+delRow[i];
-            int ncol=col+delCol[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==iniColor && ans[nrow][ncol]!=newColor){
-                dfs(nrow,ncol,ans,image,newColor,delRow,delCol,iniColor);
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+
+        int dr[]={-1,0,1,0};
+        int dc[]={0,1,0,-1};
+
+        while(!q.empty() ){
+            auto p=q.front();
+            q.pop();
+
+            int r=p.first;
+            int c=p.second;
+
+            for(int i=0;i<4;i++){
+                int nr=dr[i]+r;
+                int nc=dc[i]+c;
+
+                if(0<=nr && nr<n && 0<=nc && nc<m && vis[nr][nc]==-1 && image[nr][nc]==original){
+                    image[nr][nc]=color;
+                    vis[nr][nc]=1;
+                    q.push({nr,nc});
+                }
             }
         }
-    }
-public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        int iniColor=image[sr][sc];
-        vector<vector<int>>ans=image;
-        int delRow[]={-1,0,1,0};
-        int delCol[]={0,1,0,-1};
-        dfs(sr,sc,ans,image,newColor,delRow,delCol,iniColor);
-        return ans;
+        return image;
     }
 };
